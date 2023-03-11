ac4d586c3e7fe0e0da26c8c032a1f42ac48eee02
jdjdpsjsjdlsk <41925758+jdjdpsjsjdlsk@users.noreply.github.com>
2023-03-11 15:04:02 +0100

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,6 @@ repositories {
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.1")
-    implementation("com.google.guava:guava:31.1-jre")
     compileOnly("io.papermc.paper:paper-api:1.18.1-R0.1-SNAPSHOT")
 }
 
