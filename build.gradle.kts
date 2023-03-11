db8c36cde108c0b6cdbf64ce6f3c7f7f37ad098f
malte <41925758+malte843@users.noreply.github.com>
2023-03-11 15:19:47 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.18.1-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.18.2")
 }
 
 task<LaunchMinecraftServerTask>("testPlugin") {
